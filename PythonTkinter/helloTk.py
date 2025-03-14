12c337e61a84ebb9c2acc16c302dd6e828d9ee7e
DelTechs01 <bleamalika@gmail.com>
2025-03-14 13:52:21 +0300

    Printing simple Hello World on Tkninter Winndow.

---
+++ PythonTkinter/helloTk.py
@@ -0,0 +1,9 @@
+import tkinter
+from tkinter import *
+root = Tk()
+#Creating a label widget
+myLabel = Label(root, text="Hello World in Tkinter!")
+#Showing it on the Screen
+myLabel.pack()
+#Creating the loop
+root.mainloop()
