a597215699799ecb040711a22cb2dea57b377905
DelTechs01 <bleamalika@gmail.com>
2025-04-08 22:47:07 +0300

    html code and logo

---
+++ govForm.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <div class="logo">
+            <img src="government-of-kenya-emblem-gok.png" alt="government-of-kenya">
+        </div>
+        <div class="formcontainer">
+            <form action="" method="POST">
+                <label for="email">Email Address or ID Number</label>
+                <input type="text" name="email" id="email" placeholder="your@example.com or 11223344">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="password">
+                <a href="forgotpassword.html">Forgot Your Password?</a>
+                <button type="submit" name="submit">Login</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
